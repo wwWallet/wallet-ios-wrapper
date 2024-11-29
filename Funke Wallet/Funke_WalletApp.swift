445e9efc8bd237b9b4e541039e54cc58d08b90e0
Jens Utbult <jens.utbult@yubico.com>
2024-11-29 12:28:59 +0100

    Initial commit.

---
+++ Funke Wallet/Funke_WalletApp.swift
@@ -0,0 +1,17 @@
+//
+//  Funke_WalletApp.swift
+//  Funke Wallet
+//
+//  Created by Jens Utbult on 2024-11-29.
+//
+
+import SwiftUI
+
+@main
+struct Funke_WalletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
