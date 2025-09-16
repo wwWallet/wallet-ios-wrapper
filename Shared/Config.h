b6cab448412adcd30cd116e9cc1422be53f835d4
Didr <9605751+Didr@users.noreply.github.com>
2025-09-16 16:24:38 +0200

    Add zkp.app.siros.org as 5th environment

--- Shared/Config.h
+++ Shared/Config.h
@@ -21,6 +21,8 @@ NS_ASSUME_NONNULL_BEGIN
 
 @property (class, nonatomic, assign, readonly, nonnull) NSString *baseDomain4 NS_REFINED_FOR_SWIFT;
 
+@property (class, nonatomic, assign, readonly, nonnull) NSString *baseDomain5 NS_REFINED_FOR_SWIFT;
+
 @end
 
 NS_ASSUME_NONNULL_END
